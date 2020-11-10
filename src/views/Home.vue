92099e27332e9b1bc247de543e4c6051f6f7f576
Dmitry Kushnerevich <kushich.dmitry@gmail.com>
2020-11-10 22:04:27 +0300

    add: table

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,94 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <v-app id="inspire">
+  <v-card>
+    <v-card-title>
+      <v-text-field
+        v-model="search"
+        append-icon="mdi-magnify"
+        label="Search"
+        single-line
+        hide-details
+      ></v-text-field>
+    </v-card-title>
+    <v-data-table
+      :headers="headers"
+      :items="students"
+      :search="search"
+    ></v-data-table>
+  </v-card>
+  </v-app>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 @Component({
   components: {
-    HelloWorld,
   },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  search = ''
+  headers = [
+    {
+      text: 'ФИО',
+      align: 'start',
+      sortable: false,
+      value: 'student',
+    },
+    { text: 'Дата рождения', value: 'dateOfBirth' },
+    { text: 'Вид спорта', value: 'sportType' },
+    { text: 'Квалификация', value: 'qualification' },
+    { text: 'Специализация', value: 'specification' },
+    { text: 'Дата обследования', value: 'dateOfExamination' },
+    { text: 'Место обследования', value: 'placeOfExamination' },
+  ]
+
+  students = [
+    {
+      student: 'Зубенко Михаил Петрович',
+      dateOfBirth: '14/12/1999',
+      sportType: 'Футбол',
+      qualification: '1-й разряд',
+      specification: 'футбол',
+      dateOfExamination: '21.11.2020',
+      placeOfExamination: 'Медицинский центр "Волна"',
+    },
+    {
+      student: 'Зубенко Михаил Петрович',
+      dateOfBirth: '14/12/1999',
+      sportType: 'Рукабол',
+      qualification: '1-й разряд',
+      specification: 'футбол',
+      dateOfExamination: '21.11.2020',
+      placeOfExamination: 'Медицинский центр "Волна"',
+    },
+    {
+      student: 'Зубенко Михаил Петрович',
+      dateOfBirth: '14/12/1999',
+      sportType: 'Футбол',
+      qualification: '2-й разряд',
+      specification: 'футбол',
+      dateOfExamination: '21.11.2020',
+      placeOfExamination: 'Медицинский центр "Волна"',
+    },
+    {
+      student: 'Зубенко Михаил Петрович',
+      dateOfBirth: '14/12/1999',
+      sportType: 'Рукабол',
+      qualification: '2-й разряд',
+      specification: 'футбол',
+      dateOfExamination: '21.11.2020',
+      placeOfExamination: 'Медицинский центр "Волна"',
+    },
+    {
+      student: 'Зубенко Михаил Петрович',
+      dateOfBirth: '14/12/1999',
+      sportType: 'Футбол',
+      qualification: '1-й разряд',
+      specification: 'футбол',
+      dateOfExamination: '21.11.2020',
+      placeOfExamination: 'Медицинский центр "Волна"',
+    },
+  ]
+}
 </script>
